cd02312962498321a8d9ac72589e5e40fc11ab9b
Shawn Averkamp <shawnaverkamp@gmail.com>
2017-01-13 13:58:04 -0500

    update item types with requestable property

--- scripts/serialize-catalogItemTypes.py
+++ scripts/serialize-catalogItemTypes.py
@@ -1,6 +1,7 @@
-# script to convert catalogItemTypes.csv to json-ld
+# script to convert locations.csv to json-ld
 # requires libraries/plugins: rdflib, rdflib-jsonld
 
+from rdflib.namespace import RDF, SKOS
 from rdflib import Namespace, Graph, plugin
 from rdflib.serializer import Serializer
 import rdflib
@@ -22,22 +23,29 @@ for r in reader:
     notation = rdflib.Literal(r['skos:notation'])
     locationType = r['nypl:locationType'].split(';')
     note = r['skos:note']
-    catalogItemType = nyplCatalogItemType + str(id)   
+    requestable = r['nypl:requestable']
+    catalogItemType = nyplCatalogItemType + str(id)
+    
     g.add( (catalogItemType, RDF.type, nypl.CatalogItemType))
     g.add( (catalogItemType, SKOS.prefLabel, preflabel))
     g.add( (catalogItemType, SKOS.notation, notation))
     if note != '':
         note = rdflib.Literal(note)
         g.add ( (catalogItemType, SKOS.note, note) )
+    if requestable != '':
+        requestable = rdflib.Literal(requestable, datatype="XSD:boolean")
+        g.add ( (catalogItemType, nypl.requestable, requestable) )
     for l in locationType:
         if l != '':
             l = rdflib.Literal(l)
             g.add( (catalogItemType, nypl.locationType, l))
 
-z = open('sierra-codes-catalogItemTypes.json', 'wb')
+z = open(catalogItemTypes.json', 'wb')
 
 context = {"nypl": "http://data.nypl.org/nypl-core/",
            "skos": "http://www.w3.org/2004/02/skos/core#",
            "nyplCatalogItemTypes": "http://data.nypl.org/catalogItemTypes/"}
 z.write(g.serialize(format="json-ld", context=context))
+
 z.close()
+f.close()
