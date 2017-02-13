e41cc107016c519164210f2300c092e6667a7784
Paul Beaudoin <contact@paulbeaudoin.com>
2017-02-13 15:22:12 -0500

    better error handling (save offending marcjson to cache for inspection)

--- jobs/update-items.js
+++ jobs/update-items.js
@@ -8,7 +8,8 @@ var argv = require('optimist')
   .usage('Usage: $0 [--offset=num] [--limit=num]')
   .describe('offset', 'Start at index')
   .describe('limit', 'Limit to this number of records')
-  .describe('uri', 'Process specific item (from cache)')
+  .describe('uri_cache', 'Process specific item by prefixed uri (from cache)')
+  .describe('uri_seek', 'Process a specific item by non-prefixed uri (by skipping over everything else in the stream)')
   .describe('loglevel', 'Specify log level (default error)')
   .describe('threads', 'Specify number of threads to run it under')
   .describe('disablescreen', 'If running multi-threaded, disables default screen takeover')
@@ -19,13 +20,13 @@ var opts = {
   skip: parseInt(argv.skip) || 0,
   offset: parseInt(argv.offset) || 0,
   limit: parseInt(argv.limit) || 0,
-  seek: argv.seek || null
+  seek: argv.uri_seek || null
 }
 
 log.setLevel(argv.loglevel || 'error')
 
-if (argv.uri) {
-  ; (new ItemsUpdater()).item(argv.uri)
+if (argv.uri_cache) {
+  ; (new ItemsUpdater()).item(argv.uri_cache)
 } else if (argv.threads) {
   console.log('threaded')
   ItemsUpdater.threaded(argv)
