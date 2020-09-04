6c90f6cd216bc7d05bf8ba975215ccef691a4e5f
Mike Benowitz <mwbenowitz@gmail.com>
2020-09-04 13:19:39 -0400

    SCC-2206 Add Holding class to discovery-store-poster
    This creates a set of new classes and functions for handling Holding records coming from the new Holding pipeline. These represent serial holdings that are to be represented in the SCC front-end.
    
    These records function very similarly to Item records, in that they are related to Bibs in a (generally) one-to-many relationship. The records themselves are fairly simple and the a new mapping is being created in NYPL-core to represent them.
    
    In addition to adding the new Holding updater this adds some new logic to the index to properly route all new records.
    
    No new tests have been created yet, these will be added soon, but wanted to validate this general approach to extending the store-poster before delving into those issues

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var log = null
 
 const BibsUpdater = require('./lib/bibs-updater')
 const ItemsUpdater = require('./lib/items-updater')
+const HoldingUpdater = require('./lib/holdings-updater')
 const avro = require('avsc')
 const db = require('./lib/db')
 const kmsHelper = require('./lib/kms-helper')
@@ -20,6 +21,12 @@ var opts = {
   offset: 0,
   limit: 0,
   seek: null
+}
+
+const updaters = {
+  Bib: BibsUpdater,
+  Item: ItemsUpdater,
+  Holding: HoldingUpdater
 }
 
 function getSchema (schemaName) {
@@ -40,18 +47,19 @@ function getSchema (schemaName) {
 }
 
 function processEvent (event, context, callback) {
-  let bibOrItem = null
+  let bibItemOrHolding = null
 
   // Determine whether event has Bibs or Items by checking end of eventSourceARN string:
-  if (/\/Bib/.test(event.Records[0].eventSourceARN)) bibOrItem = 'Bib'
-  if (/\/Item/.test(event.Records[0].eventSourceARN)) bibOrItem = 'Item'
+  if (/\/Bib/.test(event.Records[0].eventSourceARN)) bibItemOrHolding = 'Bib'
+  if (/\/Item/.test(event.Records[0].eventSourceARN)) bibOrItemOrHolding = 'Item'
+  if (/\/Holding/.test(event.Records[0].eventSourceARN)) bibOrItemOrHolding = 'Holding'
 
   // Fail if the eventSourceARN didn't tell us what we're handling
-  if (!bibOrItem) throw new Error('Unrecognized eventSourceARN. Aborting. ' + event.Records[0].eventSourceARN)
+  if (!bibItemOrHolding) throw new Error('Unrecognized eventSourceARN. Aborting. ' + event.Records[0].eventSourceARN)
 
-  log.debug('Using schema: ', bibOrItem)
+  log.debug('Using schema: ', bibItemOrHolding)
   // db.connect().then(() => getSchema(bibOrItem)).then((schemaType) => {
-  getSchema(bibOrItem).then((schemaType) => {
+  getSchema(bibItemOrHolding).then((schemaType) => {
     // Get array of decoded records:
     var decoded = event.Records.map((record) => {
       const kinesisData = new Buffer(record.kinesis.data, 'base64')
@@ -60,7 +68,7 @@ function processEvent (event, context, callback) {
     log.debug('Processing ' + bibOrItem + ' records: ', decoded)
 
     // Invoke appropriate updater:
-    var updater = bibOrItem === 'Bib' ? (new BibsUpdater()) : (new ItemsUpdater())
+    var updater = new updaters[bibItemOrHolding]()
     updater
       .update(opts, decoded)
       .then(() => {
