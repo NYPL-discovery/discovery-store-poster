798d3f9650fad25b889423811a0eb464bf821e4e
Paul Beaudoin <contact@paulbeaudoin.com>
2021-10-06 14:23:26 -0400

    Adds support for excluding queried subfields
    Updates SierraRecord#varField to add support for excluding specific
    subfields.
    
    For example, the following retrieves a single string composed of all
    subfields in marc 791 without subfields $0 and $6:
    
    `bib.varField('791', null, { excludeSubfields: [ '0', '6'])`

--- test/sierra-record-test.js
+++ test/sierra-record-test.js
@@ -27,4 +27,53 @@ describe('SierraRecord', function () {
       expect(title[0]).to.eq('ʻOrekh ha-din be-Yiśraʾel : maʻamado, zekhuyotaṿ ṿe-ḥovotaṿ : leḳeṭ dinim ṿe-halakhot / ba-ʻarikhat S. Ginosar.')
     })
   })
+
+  describe('varField', function () {
+    it('able to return tagged subfields', function () {
+      const record = new SierraRecord(require('./data/bib-10001936.json'))
+      // Get title using all subfields
+      const title = record.varField('245', null, { tagSubfields: true })
+      expect(title).to.be.a('array')
+      expect(title[0]).to.deep.include({
+        a: 'Niwtʻer azgayin patmutʻian hamar',
+        b: 'Ereveli hay kazunkʻ ; Parskastan /',
+        c: 'Ashkhatasirutʻiamb Galust Shermazaniani.'
+      })
+    })
+
+    it('able to return tagged subfields with certain subfields excluded', function () {
+      const record = new SierraRecord(require('./data/bib-10001936.json'))
+      // Get title using all subfields
+      const title = record.varField('245', null, { tagSubfields: true, excludeSubfields: ['b'] })
+      expect(title).to.be.a('array')
+      expect(title[0]).to.deep.include({
+        a: 'Niwtʻer azgayin patmutʻian hamar',
+        c: 'Ashkhatasirutʻiamb Galust Shermazaniani.'
+      })
+    })
+
+    it('able to return single string from all subfields', function () {
+      const record = new SierraRecord(require('./data/bib-10001936.json'))
+      // Get title using all subfields
+      const title = record.varField('245')
+      expect(title).to.be.a('array')
+      expect(title[0]).to.eq('Niwtʻer azgayin patmutʻian hamar Ereveli hay kazunkʻ ; Parskastan / Ashkhatasirutʻiamb Galust Shermazaniani.')
+    })
+
+    it('able to return single string from all subfields, excluding some subfields', function () {
+      const record = new SierraRecord(require('./data/bib-10001936.json'))
+      // Get title using all subfields
+      const title = record.varField('245', null, { excludeSubfields: ['b'] })
+      expect(title).to.be.a('array')
+      expect(title[0]).to.eq('Niwtʻer azgayin patmutʻian hamar Ashkhatasirutʻiamb Galust Shermazaniani.')
+    })
+
+    it('able to return single string from certain subfields', function () {
+      const record = new SierraRecord(require('./data/bib-10001936.json'))
+      // Get title using all subfields
+      const title = record.varField('245', ['a', 'c'], { excludeSubfields: ['b'] })
+      expect(title).to.be.a('array')
+      expect(title[0]).to.eq('Niwtʻer azgayin patmutʻian hamar Ashkhatasirutʻiamb Galust Shermazaniani.')
+    })
+  })
 })
