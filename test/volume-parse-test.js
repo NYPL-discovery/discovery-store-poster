67e1f88ae9eecf3083f1ae18cde631441493cda2
Vera Kahn <verakahn@nypl.org>
2022-11-10 10:17:39 -0500

    refactor volume parser

--- test/volume-parse-test.js
+++ test/volume-parse-test.js
@@ -2,7 +2,7 @@
 const expect = require('chai').expect
 const { parseVolume } = require('../lib/volume-parser')
 
-describe('volume parsing', () => {
+describe.only('volume parsing', () => {
   it('v. 36-37 (Nov. 1965-Oct. 1967)', () => {
     expect(parseVolume('v. 36-37 (Nov. 1965-Oct. 1967)')).to.deep.equal([[36, 37]])
   })
